8e207d5f97d3a10ba64dac8a196a9fb6ab44953a
sharmilaperiyasamy <sharmilaperiyasamy1992@gmail.com>
2022-07-25 12:50:55 +0530

    [Sharmila] modify employee payroll class with new attributes and getters and setters

--- JS/Employee.js
+++ JS/Employee.js
@@ -1,31 +1,47 @@
 class Employee{
-    id;
-    name;
-    salary;
-    gender;
-    startDate;
+    //getter and setter methods
+    get id() {return this._id;}
+    set id(id){
+        this._id=id;
+    }
+    get name() {return this._name; }
+    set name(name) {
+    let nameRegex=RegExp('^[A-Z]{1}[a-zA-z]{2,}$');
+    if(nameRegex.test(name))
+    this._name=name;
+    else
+    throw 'Name is incorrect!'
+    }
+    get profilePic() {return this._profilePic;}
+    set profilePic(profilePic){
+        this._profilePic=profilePic;
+    }
+    get gender() {return this._gender;}
+    set gender(gender){
+        this._gender=gender;
+    }
+    get department() {return this._department;}
+    set department(department){
+        this._department=department;
+    }
+    get salary() {return this._salary;}
+    set salary(salary){
+        this._salary=salary;
+    }
+    get startDate() {return this._startDate;}
+    set startDate(startDate){
+        this._startDate=startDate;
+    }
+    get note() {return this._note;}
+    set note(note){
+        this._note=note;
+    }
 
-constructor(...params){
-    this.id=params[0];
-    this.name=params[1];
-    this.salary=params[2];
-    this.gender=params[3];
-    this.startDate=params[4];
-}
-// //getter and setter methods
-// get name() {return this._name; }
-// set name(name) {
-//     let nameRegex=RegExp('^[A-Z]{1}[a-z]{3,}$');
-//     if(nameRegex.test(name))
-//     this._name=name;
-//     else
-//     throw 'Name is incorrect!';
-// }
 //method
 toString() {
     const options = { year: 'numeric', month:'long',day:'numeric'};
     const empDate = !this.startDate ? "undefined":this.startDate.toLocaleDateString("en-US",options);
-    return "id=" + this.id+", name="+this.name+",salary=" + this.salary+", "+"gender="+this.gender+
-    ", startDate="+empDate;
+    return "id=" + this.id+", name="+this.name+", profilePic="+this.profilePic+", gender="+this.gender
+    +", department="+this.department+", salary=" + this.salary+", startDate="+empDate+", note="+this._note;
 }
 }
